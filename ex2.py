901ce57262ad013b44b65f4625ecfbf5d1b81fe0
Jose Eduardo Jaramillo Barrera <lalo8874@hotmail.com>
2022-05-17 11:40:34 -0700

    Create files in repository to practice python

---
+++ ex2.py
@@ -0,0 +1,15 @@
+print(' ')
+print("")
+print("Hello world!")
+print("Hello        wold")
+print("__________________________________")
+space = ' '
+
+'''
+    2 spaces would be here
+    1 space after first parameter
+    1 space by using space variable
+    1 space after second parameter
+'''
+
+print("Hello", space*5, "world")
