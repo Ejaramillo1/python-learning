ab17013f502bee77bf3507f5d26b6eade1849a27
Eduardo <ejaramillobarrera@gmail.com>
2022-05-21 14:12:16 -0700

    adding excersice 7:

---
+++ python-megacourse/ex7.py
@@ -0,0 +1,19 @@
+your_name="THIs is the variable of the string"
+print(your_name)
+your_name1="""This is the Variable of
+Multiple type 
+string """
+
+print(your_name1)
+
+a="python"
+b="3"
+c=a+b
+print(c)
+
+d = "This is python 3"
+
+print("this outPUt %s"%d)
+
+
+my_string = ""
