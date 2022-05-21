fc1704dc58e642f9f49440959e1d8e13ba0ce5c1
Eduardo <ejaramillobarrera@gmail.com>
2022-05-21 13:34:25 -0700

    Adding files

---
+++ python-megacourse/ex4.py
@@ -0,0 +1,8 @@
+a=10
+b=20
+print(a,b)
+a,b = b,a
+
+
+print("This is the value of a ", a)
+print("This is the value of b ", b)
---
+++ python-megacourse/ex5.py
@@ -0,0 +1,7 @@
+def abc():
+    var1="Python" # This is a variable declare under local scope
+    print(var1)
+    
+
+
+abc()
