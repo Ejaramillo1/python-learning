1d246a5d134858bdd4bc573c7d788f0729622ca4
Jose Eduardo Jaramillo Barrera <ejaramillobarrera@gmail.com>
2022-06-06 15:11:16 -0700

    changing files from carpeta

---
+++ mitPython/examples/ex23.py
@@ -0,0 +1,7 @@
+total = 0
+current = 1
+
+for i in range(current, 6 + 1):
+    total += current
+    current += 1
+print(total)
---
+++ mitPython/examples/ex24.py
@@ -0,0 +1,12 @@
+from re import X
+
+
+x = 3
+ans = 0
+
+itersLeft = x
+
+while (itersLeft != 0):
+    ans = ans + x
+    itersLeft = itersLeft - 1
+print(str(x) + '*' + str(x) + ' = ' + str(ans))
